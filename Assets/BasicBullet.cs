4d4e7b71695a78dceb0081987acfd5d9cabbd845
AndyZhu <zhuyidiwow@gmail.com>
2016-10-15 19:18:58 +0800

    prototype
    imported Prototyping standard assets and set up the prototype

---
+++ Assets/BasicBullet.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class BasicBullet : MonoBehaviour {
+
+    public float damage;
+    public float speed;
+
+    private Vector3 shootDirection;
+
+    void FixedUpdate() {
+        transform.Translate(shootDirection.normalized * speed * Time.deltaTime, Space.World);
+    }
+
+	public void ShootToRay(Ray shootRay) {
+        this.shootDirection = shootRay.direction;
+        this.transform.position = shootRay.origin;
+        //RotateToShootDirection();
+        Debug.Log("Actual shoot direction: " + shootDirection);
+    }
+
+    void RotateToShootDirection() {
+        Vector3 newRotation = Vector3.RotateTowards(transform.forward, shootDirection, 0.01f, 0.0f);
+        transform.rotation = Quaternion.LookRotation(newRotation, Vector3.up);
+    }
+}
