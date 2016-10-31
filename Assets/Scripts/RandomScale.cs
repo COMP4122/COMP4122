447c13e22e1fde528eb58a623c939be38600ebfe
AndyZhu <zhuyidiwow@gmail.com>
2016-10-31 21:59:13 +0800

    Stonehenge and Camp completed
    also made some handy prefabs for the Nature Pack

---
+++ Assets/Scripts/RandomScale.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class RandomScale : MonoBehaviour {
+
+    private float maxScale = 1.5f;
+    private float minScale = 0.75f;
+	// Use this for initialization
+	void Start () {
+		transform.localScale *= Random.Range(minScale, maxScale);
+        //transform.Rotate(0, Random.Range(0, 180f), 0);
+	}
+}
