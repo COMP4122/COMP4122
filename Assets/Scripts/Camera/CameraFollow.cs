aa24175bee8c9f76b1cc183e8826550a1635e471
AndyZhu <zhuyidiwow@gmail.com>
2016-11-04 16:39:06 +0800

    camera horizontal movement

--- Assets/Scripts/Camera/CameraFollow.cs
+++ Assets/Scripts/Camera/CameraFollow.cs
@@ -4,8 +4,20 @@ using System.Collections;
 public class CameraFollow : MonoBehaviour {
 
     public float distance;
-    public float angle;
+    public float verticalAngle;
+    public float horizontalAngle;
     public Transform followObject;
+
+    #region rotation variable
+    private float horizontalDistance = 0;
+    private float xOffset;
+    private float yOffset;
+    private float zOffset;
+
+    #endregion
+    void Update() {
+        
+    }
 
 	void LateUpdate () {
         MoveToTarget();
@@ -13,10 +25,14 @@ public class CameraFollow : MonoBehaviour {
 	}
 
     void MoveToTarget() {
+        yOffset = distance * Mathf.Sin(Mathf.Deg2Rad * verticalAngle);
+        horizontalDistance = distance * Mathf.Cos(Mathf.Deg2Rad * verticalAngle);
+        xOffset = horizontalDistance * Mathf.Sin(Mathf.Deg2Rad * horizontalAngle);
+        zOffset = -horizontalDistance * Mathf.Cos(Mathf.Deg2Rad * horizontalAngle);
         this.transform.position = new Vector3(
-                followObject.position.x,
-                followObject.position.y + distance * Mathf.Sin(Mathf.Deg2Rad * angle),
-                followObject.position.z - distance * Mathf.Cos(Mathf.Deg2Rad * angle)
+                followObject.position.x + xOffset,
+                followObject.position.y + yOffset,
+                followObject.position.z + zOffset
         );
     }
 
