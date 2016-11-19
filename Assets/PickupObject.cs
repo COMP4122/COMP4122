c51abdf4d237ad674e776eb561b597381c73f5e9
AndyZhu <zhuyidiwow@gmail.com>
2016-11-19 19:48:58 +0800

    finally got picking up working

---
+++ Assets/PickupObject.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickupObject : MonoBehaviour {
+
+
+    private Animator animator;
+
+    void Start() {
+        animator = GetComponent<Animator>();
+        animator.SetBool("PickingUpObjects", false);
+    }
+
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.F)) {
+            StartCoroutine(PickUpObjectCoroutine());
+        }
+    }
+
+    IEnumerator PickUpObjectCoroutine() {
+        animator.SetBool("PickingUpObjects", true);
+        yield return new WaitForSeconds(1.5f);
+        animator.SetBool("PickingUpObjects", false);
+        StopCoroutine("PickUpObjectCoroutine");
+    }
+
+}
