e86c453e22206e5da96bc9c200f27219ad254759
AndyZhu <zhuyidiwow@gmail.com>
2016-11-20 19:17:43 +0800

    Liu Chang's work

---
+++ Assets/Scripts/Forest/ForestStart.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ForestStart : MonoBehaviour {
+
+    public Camera playerplay;
+    public Camera sceneplay;
+    public GameObject deerAnimated;
+    public GameObject deer;
+
+    void Start () {
+        deerAnimated.SetActive(true);
+        deer.SetActive(false);
+        playerplay.enabled = false;
+        sceneplay.enabled = true;
+        StartCoroutine(Switch());
+    }
+
+    IEnumerator Switch()
+    {
+        yield return new WaitForSeconds(3.0f);
+        sceneplay.enabled = false;
+        playerplay.enabled = true;
+        yield return new WaitForSeconds(3.0f);
+        deerAnimated.SetActive(false);
+        deer.SetActive(true);
+    }
+
+}
